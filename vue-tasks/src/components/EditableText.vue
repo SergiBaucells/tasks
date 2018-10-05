05bca575858c57cecae78b1eaa6c6cc74d17cc59
SergiBaucells <sergibaucells@iesebre.com>
2018-10-05 16:57:44 +0200

    Edit OK!!!

--- vue-tasks/src/components/EditableText.vue
+++ vue-tasks/src/components/EditableText.vue
@@ -1,10 +1,21 @@
 <template>
     <span>
         <span v-if="!editing" @dblclick="editing=true">
-            <slot></slot>
+            {{currentText}}
+            <svg @click="editing=true" class="h-3 w-3 cursor-pointer" xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 20 20">
+                <path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/>
+            </svg>
         </span>
         <span v-if="editing" @keyup.esc="editing=false" @keyup.enter="edit">
-            <input type="text" value="">
+            <input type="text" v-model="currentText">
+            <svg @click="edit" class="h-3 w-3 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/>
+            </svg>
+            <svg @click="editing=false" class="h-3 w-3 cursor-pointer" xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 20 20">
+                <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/>
+            </svg>
         </span>
     </span>
 </template>
@@ -14,13 +25,22 @@
         name: 'EditableText',
         data() {
             return {
-                editing: false
+                editing: false,
+                currentText: this.text
             }
         },
+        props: {
+            'text': {
+                type: String,
+                required: true
+            }
+        },
+        // props: ['text'],
         methods: {
             edit() {
-                window.console.log('TODO EDIT');
                 this.editing = false
+                // Informar al pare
+                this.$emit('edited', this.currentText)
             }
         }
     }
--- vue-tasks/src/components/Tasks.vue
+++ vue-tasks/src/components/Tasks.vue
@@ -16,13 +16,10 @@
                 <!--<li v-else>{{task.name}}</li>-->
                 <li v-for="task in filteredTasks" :key="task.id" class="text-grey-darker m-2 pl-5">
                     <span :class="{strike:task.completed}">
-                        <editable-text>
-                            {{task.name}}
+                        <editable-text :text="task.name" @edited="editName(task, $event)">
+                            <!--{{task.name}}-->
                         </editable-text>
                     </span>
-                    <svg class="h-4 w-4 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/>
-                    </svg>
                     &nbsp;
                     <span @click="remove(task)" class="cursor-pointer">&#215;</span>
                 </li>
@@ -88,6 +85,12 @@
             }
         },
         methods: {
+            editName(task, text) {
+                // console.log('TASK: ', task.name);
+                // console.log('TEXT: ', text);
+                // console.log("TODO editName");
+                task.name = text
+            },
             setFilter(newFilter) {
                 this.filter = newFilter
             },

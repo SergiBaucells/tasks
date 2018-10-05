aaf2016234949382c86101448be65dbb0c23e14c
SergiBaucells <sergibaucells@iesebre.com>
2018-10-05 17:35:24 +0200

    Estil OK!!!

--- vue-tasks/src/components/Tasks.vue
+++ vue-tasks/src/components/Tasks.vue
@@ -6,7 +6,8 @@
                 <input type="text" v-model="newTask" @keyup.enter="add" placeholder="Nova Tasca"
                        class="m-3 mt-5 p-2 pl-5 shadow border rounded focus:outline-none focus:shadow-outline text-grey-darker">
                 <!--<button @click="add">Afegir</button>-->
-                <svg @click="add" class="h-4 w-4 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <svg @click="add" class="h-4 w-4 cursor-pointer fill-current text-green"
+                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"/>
                 </svg>
             </div>
@@ -20,23 +21,22 @@
                             <!--{{task.name}}-->
                         </editable-text>
                     </span>
-                    &nbsp;
-                    <span @click="remove(task)" class="cursor-pointer">&#215;</span>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" @click="remove(task)"
+                         class="h-4 w-4 cursor-pointer ml-3 mt-1 fill-current text-red" viewBox="0 0 20 20">
+                        <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zM11.4 10l2.83-2.83-1.41-1.41L10 8.59 7.17 5.76 5.76 7.17 8.59 10l-2.83 2.83 1.41 1.41L10 11.41l2.83 2.83 1.41-1.41L11.41 10z"/>
+                    </svg>
                 </li>
             </ul>
-            <h3>Filtros</h3>
-            Active filter: {{filter}}
-            <ul class="list-reset">
-                <li>
-                    <button @click="setFilter('all')">Totes</button>
-                </li>
-                <li>
-                    <button @click="setFilter('completed')">Completades</button>
-                </li>
-                <li>
-                    <button @click="setFilter('active')">Pendents</button>
-                </li>
-            </ul>
+            <div>
+                <h3 class="mb-3 mt-2">Filtros</h3>
+                Active filter: {{filter}}
+                <div class="mt-4">
+                <button @click="setFilter('all')">Totes</button>
+                <button @click="setFilter('completed')" class="ml-3 mr-3">Completades</button>
+                <button @click="setFilter('active')">Pendents</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -100,7 +100,6 @@
                 this.newTask = ""
             },
             remove(task) {
-                window.console.log(task)
                 this.tasks.splice(this.tasks.indexOf(task), 1)
             }
         }

73a664a78ccb1380b8db3e3c369f9cfd23dcb129
SergiBaucells <sergibaucells@iesebre.com>
2019-01-08 19:16:43 +0100

    Sync

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="user" content="{{ logged_user() }}">
     <meta name="git" content="{{ git() }}">
+    <link rel="manifest" href="/manifest.json">
     <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
     <link rel="shortcut icon" type="image/png"
           href="https://twibbon.blob.core.windows.net/twibbon/2014/303/d9b1e1c1-e3ea-4e6f-9daf-0c6e2d10dab0.png">

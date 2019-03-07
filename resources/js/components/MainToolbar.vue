7917ad39d978c159e2d07f912fde40e0d09d2ccb
SergiBaucells <sergibaucells@iesebre.com>
2019-03-07 18:33:50 +0100

    Sync

--- resources/js/components/MainToolbar.vue
+++ resources/js/components/MainToolbar.vue
@@ -9,8 +9,8 @@
         <span class="mr-4 hidden-xs-only" v-role="'SuperAdmin'"><git-info></git-info></span>
 
         <v-avatar @click="$emit('toggle-right')" :title="user.name">
-            <img v-if="user.online" style="border: lawngreen 2px solid; margin: 20px;" :src="'https://www.gravatar.com/avatar/'+ user.gravatar" alt="avatar">
-            <img v-else style="border: red 2px solid; margin: 20px;" :src="'https://www.gravatar.com/avatar/' + user.gravatar" alt="avatar">
+            <img v-if="user.online" style="border: lawngreen 2px solid; margin: 20px;" :src=userAvatar alt="avatar">
+            <img v-else style="border: red 2px solid; margin: 20px;" :src=userAvatar alt="avatar">
         </v-avatar>
         <v-form action="logout" method="POST">
             <v-btn type="submit" icon><v-icon>exit_to_app</v-icon></v-btn>
@@ -27,6 +27,11 @@ export default {
   components: {
     'notifications-widget': NotificationsWidget,
     'git-info': GitInfoComponent
+  },
+  data () {
+    return {
+      userAvatar: window.laravel_user.gravatar
+    }
   },
   created () {
     this.user = window.laravel_user

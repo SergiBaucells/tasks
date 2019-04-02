6031eb4f4fa706f264c6f86c4cedbb5643484e7b
SergiBaucells <sergibaucells@iesebre.com>
2019-04-02 15:32:01 +0200

    Sync

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts("/service-worker/precache-manifest.184d615c1114b1a881a3f5692ce6b1aa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.1.0/workbox-sw.js");
+importScripts("/service-worker/precache-manifest.ef66bffdb7db6aaed0bb581b212ff6ac.js", "https://storage.googleapis.com/workbox-cdn/releases/4.1.0/workbox-sw.js");
 
 workbox.setConfig({
   debug: true
